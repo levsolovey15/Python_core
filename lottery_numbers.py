5994389316ac17e71f514f3a162b985270d86c63
LevSolovey <levsolovey15@gmail.com>
2025-11-02 10:26:00 +0100

    Solving of the 2 task

---
+++ lottery_numbers.py
@@ -0,0 +1,18 @@
+import random
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
+
+    if (
+        min < 1 or 
+        max > 1000 or 
+        quantity < 1 or 
+        quantity > (max - min + 1)
+    ):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+
+    return sorted(numbers)
+
+lottery_numbers = get_numbers_ticket(1, 39, 5)
+print("Ваші лотерейні числа:", lottery_numbers)
